cad033475ea38972c4de01d121c7faa37b909d82
Alexandre Bergel <abergel@dcc.uchile.cl>
2021-06-09 21:46:35 +0200

    Update runTests.sh

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -1,5 +1,5 @@
 curl -L https://get.pharo.org/64/ | bash
-./pharo --headless Pharo.image ./scripts/runTest.st
+./pharo --headless Pharo.image ./scripts/runTests.st
 
 FILE=/tmp/result.txt
 if [ ! -f "$FILE" ]; then
